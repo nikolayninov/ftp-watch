9f475c23697d7c7816012c8cf1ac78fd835ebb97
nikolayninov <nikininov1@gmail.com>
2019-08-10 23:46:46 +0300

    Addin check.py

---
+++ check.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import win32com.shell.shell as shell
+import winsound
+import ftplib as ftp
+import ssl
+import sched
+import time
+from pygame import mixer
+
+HOST = 'ftp.dlptest.com'
+PORT = 12121
+USER = 'dlpuser@dlptest.com'
+PwD = 'fLDScD4Ynth0p4OJ6bW6qCxjh'
+SOUND_URL = 's.mp3'
+mixer.init()
+mixer.music.load(SOUND_URL)
+
+
+def connect():
+    ftps = ftp.FTP_TLS()
+    # ftps.ssl_version = ssl.PROTOCOL_TLSv1_2
+    ftps.debugging = 2
+    ftps.connect(HOST)  # YOU CAN ADD SPECIFIC PORT
+    ftps.login(USER, PwD)
+    print("Connection established!")
+    return ftps
+
+
+data = ""
+
+
+def check(initial=False):
+    global data
+    mixer.music.stop()
+    with connect() as ftp:
+        new_data = ''.join(ftp.nlst())
+        if not initial:
+            print("DATA", data)
+            print('NEW DATA', new_data)
+            if data != new_data:
+                print("GOTCHA")
+                mixer.music.play()
+        data = new_data
+        ftp.quit()
+
+
+def run_as_admin():
+    ASADMIN = 'asadmin'
+
+    if sys.argv[-1] != ASADMIN:
+        script = os.path.abspath(sys.argv[0])
+        params = ' '.join([script] + sys.argv[1:] + [ASADMIN])
+        shell.ShellExecuteEx(
+            lpVerb='runas', lpFile=sys.executable, lpParameters=params)
+        print("I am root now.")
+        check()
+
+
+s = sched.scheduler(time.time, time.sleep)
+
+
+def repeat(sc):
+    check()
+    s.enter(5, 1, repeat, (sc,))
+
+
+check(True)
+s.enter(5, 1, repeat, (s,))
+s.run()
+
+# run_as_admin()
